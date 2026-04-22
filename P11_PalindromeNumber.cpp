5741892055b03902dd0963d6e73c4a3b4a77f8a1
hossam101 <alhdadhsamtlt@gmail.com>
2026-04-22 19:29:56 +0300

    Add Problem 11: Check palindrome number

---
+++ P11_PalindromeNumber.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Reads a positive number from the user
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int ReverseNumber(int num)
+{
+    int remainder = 0, reversedNumber = 0;
+    while (num > 0)
+    {
+        remainder = num % 10;
+        num /= 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+    }
+    return reversedNumber;
+}
+
+bool IsPalindromeNumber(int number)
+{
+    return number == ReverseNumber(number);
+}
+
+int main()
+{
+    if (IsPalindromeNumber(ReadPositiveNumber("Enter a positive number : ")))
+        cout << "Yes, it is a Palindrome Number";
+    else
+        cout << "No, it is not a Palindrome Number";
+
+    return 0;
+}
