3522b178e27d4a71f3b4b72b19bd070b876103f0
hossam101 <alhdadhsamtlt@gmail.com>
2026-04-22 20:11:55 +0300

    Add Problem 12: Print inverted number pattern

---
+++ P12_InvertedNumberPattern.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Reads a positive number from the user
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+void PrintInvertedNumberPattern(int Number)
+{
+    for (int i = Number; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedNumberPattern(ReadPositiveNumber("Enter a positive number : "));
+
+    return 0;
+}
