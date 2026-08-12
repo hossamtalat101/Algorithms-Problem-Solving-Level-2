2a7bd7aa7a6c0ea46dcaaaba349fef77d594b711
hossam101 <alhdadhsamtlt@gmail.com>
2026-08-13 01:13:02 +0300

    Add Problem 44: Count positive numbers in array

---
+++ P44_CountPositiveNumbersInArray.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int Array[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        Array[i] = RandomNumber(-100, 100);
+    }
+}
+
+int CountPositiveNumbers(int Array[100], int Length)
+{
+    int Counter = 0;
+
+    for (int i = 0; i < Length; i++)
+    {
+        if (Array[i] > 0)
+            Counter++;
+    }
+
+    return Counter;
+}
+
+void PrintArray(int Array[100], int Length)
+{
+    for (int i = 0; i < Length; i++)
+        cout << Array[i] << " ";
+
+    cout << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int Array[100];
+    int ArrayLength = ReadPositiveNumber("Enter Number of elements: ");
+
+    FillArrayWithRandomNumbers(Array, ArrayLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(Array, ArrayLength);
+
+    cout << "\nPositive Numbers count is: ";
+    cout << CountPositiveNumbers(Array, ArrayLength) << endl;
+
+    return 0;
+}
